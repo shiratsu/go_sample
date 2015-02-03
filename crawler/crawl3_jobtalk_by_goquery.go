22f66c3ef673a9992724f8bbd8e772a6a5192cd3
平塚俊輔 <shun_hiratsuka2002@yahoo.co.jp>
2015-02-04 00:10:53 +0900

    shuuseiwo kommit

--- crawler/crawl3_jobtalk_by_goquery.go
+++ crawler/crawl3_jobtalk_by_goquery.go
@@ -1,39 +1,52 @@
 package main
 
+/**
+ *	勉強用にクロールするもの作ってみた
+ * 	今後はこれをDBに入れるところまで作ってみようかな
+ */
+
 import (
-     "fmt"
-     "github.com/PuerkitoBio/goquery"
+	"fmt"
+	"github.com/PuerkitoBio/goquery"
 )
 
 type Result struct {
-     Url string
-     star string
+	Url  string
+	star string
 }
 
+const (
+	PAGE_LIMIT = 1000
+)
 
-func GetPage(url string) []Result {
+var results = []Result{}
 
-	 results := []Result{}
-	 var result Result
-     doc, _ := goquery.NewDocument(url)
-     doc.Find("div.listBox").Each(func(_ int, s *goquery.Selection) {
-          url, _ := s.ChildrenFiltered("div.ttl").Find("a").Attr("href")
-          star := s.ChildrenFiltered("p.point").Find("em").Text()
-          //star := 4.0
-          
+func GetPage(url string) {
 
-          result.Url = url
-          result.star = star
-          results = append(results, result)
-     })
-     return results
+	doc, e := goquery.NewDocument(url)
+	if e == nil {
+		var result Result
+		doc.Find("div.listBox").Each(func(_ int, s *goquery.Selection) {
+			url, _ := s.ChildrenFiltered("div.ttl").Find("a").Attr("href")
+			star := s.ChildrenFiltered("div.jobContent").ChildrenFiltered("div.txt").Find("em").Text()
+
+			//star := 4.0
+			//fmt.Println(s.ChildrenFiltered("div.jobContent div.txt").Text())
+			result.Url = url
+			result.star = star
+			results = append(results, result)
+		})
+
+	}
+
 }
 
 func main() {
-     url := "http://jobtalk.jp/company/index_1.html"
-     results := GetPage(url)
-     for _, result := range results {
-          fmt.Println(result.Url)
-          fmt.Println(result.star)
-     }
+	url := "http://jobtalk.jp/company/index_1.html"
+	GetPage(url)
+
+	for _, result := range results {
+		fmt.Println(result.Url)
+		fmt.Println(result.star)
+	}
 }
